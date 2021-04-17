ffa855c678f4293b421205a32a2e44c21dcc245c
Mohamed Abdulrahman <67754936+mocode95@users.noreply.github.com>
2021-04-17 03:52:43 +0200

    second commit

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,29 +1,39 @@
-const path = require('path')
-const common = require('./webpack.common')
-const merge = require('webpack-merge')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
-module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
-    ]
-})
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "development",
+  devtool: "source-map",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new WorkboxPlugin.GenerateSW(),
+  ],
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,40 @@
-const path = require('path')
-const common = require('./webpack.common')
-const merge = require('webpack-merge')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin')
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = merge(common, {
-    mode: 'production',
-    optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    plugins: [new MiniCssExtractPlugin({ filename: '[name].css' }), new WorkboxPlugin.GenerateSW()]
-})
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "development",
+  devtool: "source-map",
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new WorkboxPlugin.GenerateSW(),
+  ],
+};
